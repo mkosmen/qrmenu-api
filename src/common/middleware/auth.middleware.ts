70673cd5bca18e393a049f1d7bcf47a3c77d6291
Mehmet KOSMEN <mkosmen@gmail.com>
2025-10-07 14:06:02 +0300

    continue

--- src/common/middleware/auth.middleware.ts
+++ src/common/middleware/auth.middleware.ts
@@ -1,11 +1,14 @@
 import { UsersService } from '@/api/users/users.service';
 import { decrypt } from '@/lib/utils';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import {
+  Inject,
   Injectable,
   NestMiddleware,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import type { Cache } from 'cache-manager';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
@@ -13,6 +16,7 @@ export class AuthMiddleware implements NestMiddleware {
   constructor(
     private readonly jwt: JwtService,
     private readonly usersService: UsersService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async use(req: Request, res: Response, next: NextFunction) {
@@ -22,10 +26,15 @@ export class AuthMiddleware implements NestMiddleware {
     }
 
     const token = decrypt(xToken);
-    const payload = await this.jwt.verifyAsync<{ email: string }>(token);
-    const user = await this.usersService.getByEmail(payload.email);
+    const { email } = await this.jwt.verifyAsync<{ email: string }>(token);
 
+    const user = await this.usersService.getByEmail(email);
     if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const cacheResult = await this.cacheManager.get<string>(email);
+    if (!cacheResult) {
       throw new UnauthorizedException();
     }
 
